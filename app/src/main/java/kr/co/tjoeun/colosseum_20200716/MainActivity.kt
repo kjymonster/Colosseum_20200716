a13a9388ecf8e9d9ed64955dd44c2f08e39e76d6
jykim <kjymonster@naver.com>
2020-07-16 20:15:59 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/MainActivity.kt
@@ -0,0 +1,21 @@
+package kr.co.tjoeun.colosseum_20200716
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+
+}
