2fd81d079a54e99fd78fc1d916b5c2612520eee0
김재영 <kjymonster@naver.com>
2020-07-20 19:52:45 +0900

    비밀번호 길이를 입력할때마다 체크해서 textView에서 보여줌

--- app/src/main/java/kr/co/tjoeun/colosseum_20200716/SignUpActivity.kt
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/SignUpActivity.kt
@@ -29,7 +29,25 @@ class SignUpActivity : BaseActivity() {
 
             override fun onTextChanged(p0: CharSequence?, start: Int, p2: Int, p3: Int) {
                 //내용 변경 완료된 시점에서 실행.
-                Log.d("비번이 입력됨", p0.toString())
+              //  Log.d("비번이 입력됨", p0.toString())
+                //입력된 글자의 길이 확인.
+                //비어있다면, "비밀번호를 입력해 주세요."
+                //8글자가 안되면, "비밀번호가 너무 짧습니다."
+                //8글자 이상이면, "사용해도 좋은 비밀번호 입니다."
+
+                val tempPw = passwordEdt.text.toString()
+
+                //if(tempPw.length == 0)
+                if(tempPw.isEmpty()){
+                    //입력 안한 경우
+                    passwordCheckResultTxt.text = "비밀번호를 입력해 주세요."
+                }else if(tempPw.length < 8){
+                    //길이가 부족한 경우
+                    passwordCheckResultTxt.text = "비밀번호가 너무 짧습니다."
+                }else{
+                    //비어있지도 않고, 길이가 짧지도 않음. 충분히 긴 비밀번호임.
+                    passwordCheckResultTxt.text = "사용해도 좋은 비밀번호 입니다."
+                }
             }
 
         })
