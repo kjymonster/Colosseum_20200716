83a0c0ca0e4fdcd9a4dc3e249da3251efa336141
김재영 <kjymonster@naver.com>
2020-07-20 21:56:19 +0900

    토픽 목록 어댑터 뼈대 작업

---
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/adapters/TopicAdapter.kt
@@ -0,0 +1,26 @@
+package kr.co.tjoeun.colosseum_20200716.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import kr.co.tjoeun.colosseum_20200716.R
+import kr.co.tjoeun.colosseum_20200716.datas.Topic
+
+class TopicAdapter(val mContext:Context,
+                   val resId : Int, val mList:List<Topic>) : ArrayAdapter<Topic>(mContext,resId,mList){
+
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val tempRow = convertView
+        if(tempRow == null){
+            tempRow = inf.inflate(R.layout.topic_list_item, null)
+        }
+
+        val row = tempRow!!
+        return row
+    }
+
+}
