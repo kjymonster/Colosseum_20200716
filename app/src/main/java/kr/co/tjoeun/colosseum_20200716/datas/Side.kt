1a2bbc5f8c939a4b3a013929a4c2e0c8c9ed133c
김재영 <kjymonster@naver.com>
2020-07-21 21:40:39 +0900

    진영 정보 클래스 + json 파싱 기능 추가

---
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/datas/Side.kt
@@ -0,0 +1,26 @@
+package kr.co.tjoeun.colosseum_20200716.datas
+
+import org.json.JSONObject
+
+class Side {
+    var id = 0
+    var title = ""
+    var voteCount = 0
+
+    companion object {
+
+        //json을 넣으면 side로 변환해주는 기능
+        fun getSideFromJson(json: JSONObject): Side {
+            val s = Side() //Side 객체 생성
+
+            //s의 데이터들을 json을 이용해서 채우기
+            s.id = json.getInt("id")
+            s.title = json.getString("title")
+            s.voteCount = json.getInt("vote_count")
+
+            return s
+
+        }
+
+    }
+}
