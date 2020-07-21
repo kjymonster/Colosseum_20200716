757aef366dff48c6513eb350557f0c4789d78d4e
김재영 <kjymonster@naver.com>
2020-07-21 19:49:14 +0900

    Topic클래스에 json변환기능 추가  => 메인 화면에서 JSONArray 파싱때 사용.

--- app/src/main/java/kr/co/tjoeun/colosseum_20200716/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/MainActivity.kt
@@ -2,6 +2,7 @@ package kr.co.tjoeun.colosseum_20200716
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kr.co.tjoeun.colosseum_20200716.datas.Topic
 import kr.co.tjoeun.colosseum_20200716.utils.ServerUtil
 import org.json.JSONObject
@@ -30,6 +31,30 @@ class MainActivity : BaseActivity(){
     fun getTopicListFromServer(){  //너무 길어서 setValues밖에서 작업
         ServerUtil.getRequestMainInfo(mContext,object :ServerUtil.JsonResponseHandler{
             override fun onResponse(json: JSONObject) {
+
+               val data = json.getJSONObject("data")
+
+                //분석기에서 topics는 대괄호 [ ] 임, == jsonArray (data에게 호출)
+                val topics = data.getJSONArray("topics")
+
+                //topics내부에는 JSONObject들이 여러개 반복으로 들어있다.
+                //그래서 JSON을 들고있는 배열이므로, JSONArray
+
+                //for문을 이용해서, topics 내부의 데이터를 하나씩 추출.
+                for(i in 0..topics.length()-1){  // .. == until (alr+enter)
+                    //i가 0부터 ~topics의 갯수 직전. (4개 : (0,1,2,3))
+
+                    //topics내부의 데이터를 JSONObject로 추출
+                    val topicObj = topics.getJSONObject(i) //topics한테 JSONObject를 꺼내옴.
+
+                    //topicObj를 Topic 형태의 객체로 변환.
+                    val topic = Topic.getTopicFromJson(topicObj)
+
+                    //변환된 객체를 목록에 추가
+                    mTopicList =
+                     //Topic(data)로 (7/21)
+                }
+
 
             }
 
--- app/src/main/java/kr/co/tjoeun/colosseum_20200716/datas/Topic.kt
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/datas/Topic.kt
@@ -1,8 +1,29 @@
 package kr.co.tjoeun.colosseum_20200716.datas
 
+import org.json.JSONObject
+
 class Topic {
     var id = 0 //0으로 넣어서 Int가 될것이라고 암시
     var title = "" //빈칸으로 둬서 String이 될것이라고 암시
     var imageUrl = ""
+
+    //json 한 덩어리를 넣으면 => Topic 객체로 변환해주는 기능
+
+     fun getTopicFromJson(json:JSONObject) : Topic{
+
+         //변환시켜줄 Topic객체를 미리 생성
+         val topic = Topic()
+
+         //만든 객체의 내용물들을 json을 이용해서 채우자.
+         topic.id = json.getInt("id") //json에서 "id"라는 이름의 Int 가져오기
+         topic.title = json.getString("title") //json에서 "title"이라는 이름의 String 가져오기
+         topic.imageUrl = json.getString("img_url") //json에서 img_url"이라는 이름의 String 가져오기
+
+         //완성된 객체를 리턴.
+         return topic
+
+         //메인 액티비티에 이어서. (07/21)
+
+     }
 
 }
