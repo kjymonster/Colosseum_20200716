7b2d6df17d60e1be049323294ee106639cdc50f8
김재영 <kjymonster@naver.com>
2020-07-23 19:54:45 +0900

    의견 남길때 서버의 응답에 따라 다른UI처리

--- app/src/main/java/kr/co/tjoeun/colosseum_20200716/EditReplyActivity.kt
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/EditReplyActivity.kt
@@ -2,6 +2,7 @@ package kr.co.tjoeun.colosseum_20200716
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_edit_reply.*
 import kotlinx.android.synthetic.main.activity_view_topic_detail.*
 import kotlinx.android.synthetic.main.activity_view_topic_detail.topicTitleTxt
@@ -29,6 +30,27 @@ class EditReplyActivity :BaseActivity() {
 
             ServerUtil.postRequestReply(mContext, mTopicId, inputContent,object : ServerUtil.JsonResponseHandler{
                 override fun onResponse(json: JSONObject) {
+
+                    val code = json.getInt("code")
+
+                    if (code == 200) {
+                        //의견 남기기에 성공하면 => 의견이 등록되었다는 토스트
+                        //작성화면 종료.
+                        runOnUiThread {
+                            Toast.makeText(mContext,"의견이 등록되었습니다.", Toast.LENGTH_SHORT).show()
+
+                            finish() //화면 자체를 종료
+                        }
+
+                    }else {
+                        //서버가 알려주는 의견 등록 사유를 화면에 토스트로 출력
+                        val message = json.getString("message")
+
+                        runOnUiThread {
+                            Toast.makeText(mContext,message,Toast.LENGTH_SHORT).show()
+                        }
+
+                    }
 
                 }
 
