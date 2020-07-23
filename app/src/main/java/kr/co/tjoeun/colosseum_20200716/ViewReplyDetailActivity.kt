ff24e8a399eb95af778b4cb61d202742112661f4
김재영 <kjymonster@naver.com>
2020-07-23 22:28:31 +0900

    의견 상세보기 화면에 의견의 id값 전달

--- app/src/main/java/kr/co/tjoeun/colosseum_20200716/ViewReplyDetailActivity.kt
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/ViewReplyDetailActivity.kt
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class ViewReplyDetailActivity :BaseActivity() {
+
+    //보려는 의견의 id는 여러 함수에서 공유할 것 같다.
+    //그래서 멤머변수로 만들고 저장한다.
+    var mReplyId = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_reply_detail)
@@ -17,6 +22,8 @@ class ViewReplyDetailActivity :BaseActivity() {
 
     override fun setValues() {
 
+        //의견 리스트뷰에서 보내주는 id값을 멤버변수에 담아주자.
+        mReplyId = intent.getIntExtra("replyId", 0)
     }
 
 
--- app/src/main/java/kr/co/tjoeun/colosseum_20200716/adapters/ReplyAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/adapters/ReplyAdapter.kt
@@ -72,6 +72,11 @@ class ReplyAdapter (val mContext: Context,
             //Adapter는 액티비티가 아니므로, startActivity 기능을 내려주지 않는다.
             //mContext 변수가 어떤 화면이 리스트뷰를 뿌리는지 들고 있다.
             //mContext를 이용해서 액티비티를 열어주자.
+
+
+            //몇번 의견에 대한 상세를 보고싶은지 id만 넘겨주자
+            //해당 화면에서 다시 서버를 통해 데이터를 받아오자.
+            myIntent.putExtra("replyId", data.id)
             mContext.startActivity(myIntent)
 
         }
