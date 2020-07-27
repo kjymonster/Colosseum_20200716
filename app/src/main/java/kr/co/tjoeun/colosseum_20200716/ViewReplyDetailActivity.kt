2bfefbffa699e8fad4e91e30629f1edf2c33302c
jykim <kjymonster@naver.com>
2020-07-27 19:56:52 +0900

    의견 상세보기 API호출 -> 응답 처리는 스스로 작성

--- app/src/main/java/kr/co/tjoeun/colosseum_20200716/ViewReplyDetailActivity.kt
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/ViewReplyDetailActivity.kt
@@ -2,12 +2,19 @@ package kr.co.tjoeun.colosseum_20200716
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.tjoeun.colosseum_20200716.datas.Reply
+import kr.co.tjoeun.colosseum_20200716.utils.ServerUtil
+import org.json.JSONObject
 
 class ViewReplyDetailActivity :BaseActivity() {
 
     //보려는 의견의 id는 여러 함수에서 공유할 것 같다.
     //그래서 멤머변수로 만들고 저장한다.
     var mReplyId = 0
+
+    //이 화면에서 보여줘야할 의견의 정보를 가진 변수 => 멤버변수로 만들어준다.
+    //var mReply =
+    lateinit var mReply : Reply
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +31,24 @@ class ViewReplyDetailActivity :BaseActivity() {
 
         //의견 리스트뷰에서 보내주는 id값을 멤버변수에 담아주자.
         mReplyId = intent.getIntExtra("replyId", 0)
+
+        // 해당 id값에 맞는 의견 정보를 서버에서 다시 불러오자
+        getReplyFromServer()
+    }
+
+    //서버에서 의견 정보 불러오기
+    fun getReplyFromServer() {
+
+        //액티비티가 ServerUtil에게 일을 시키는 것 (ServerUtil에서는 액티비티에 일을 시키는 것)
+        ServerUtil.getRequestReplyDetail(mContext,mReplyId,object : ServerUtil.JsonResponseHandler{
+            override fun onResponse(json: JSONObject) {
+
+
+
+            }
+
+        })
+
     }
 
 
--- app/src/main/java/kr/co/tjoeun/colosseum_20200716/utils/ServerUtil.kt
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/utils/ServerUtil.kt
@@ -134,6 +134,63 @@ class ServerUtil {
 
         }
 
+        //의견 상세정보 API 호출 기능
+        fun getRequestReplyDetail(context: Context, replyId: Int, handeler: JsonResponseHandler?) {
+
+            val client = OkHttpClient()
+
+            //GET/DELETE같은 경우 : 보통 query에 파라미터를 첨부해야함.
+            //query => 주소(url)에 직접 어떤 데이터가 담기는지 기록.
+            //주소를 적을 때 파라미터 첨부도 같이 진행해야함.
+
+
+            // (07/21) 몇번 주제에 대해 보고싶은지를 화면에서 받은 topicId로 주소에 연결(파라미터로 생성해서)
+            val urlBuilder =
+                "${BASE_URL}/topic_reply/${replyId}".toHttpUrlOrNull()!!.newBuilder() //url을 가공하는 변수
+
+            //urlBuilder에 필요한 파라미터를 첨부하면 됨.
+
+
+//            urlBuilder.addEncodedQueryParameter("order_type", "NEW")
+//            urlBuilder.addEncodedQueryParameter("page_num", "1")
+
+            // 모든 데이터가 담겼으면 주소를 완성해서 String으로 저장
+            val urlString = urlBuilder.build().toString() //다 만들었다.
+
+            //실제 요청 정보를 request 변수에 종합.
+            val request = Request.Builder()
+                .url(urlString)
+                .get()
+                .header("X-Http-token", ContextUtil.getLoginUserToken(context))
+                .build()
+
+            //완성된 요청정보를 실제로 호출 -> 응답 처리
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    //서버 연결 자체에 실패한 경우
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    //연결은 성공해서, 서버가 응답을 내려줬을 때 실행됨.(아직 로그인 성공/실패가 아님)
+
+                    //실제로 서버가 내려준 응답 내용을 변수로 저장. (응답내용 = body)
+                    val bodyStr = response.body?.string() //그냥 string(), body는 null일수도 있음
+
+                    //응답 내용으로 Json 객체 생성
+                    val json = JSONObject(bodyStr)
+
+                    //서버에서 최종적으로 가져온 내용을 로그로 출력해보기
+                    Log.d("서버 응답 내용", json.toString())
+
+                    //handler 변수에 응답처리 코드가 들어있다면 실행해주자.
+                    handeler?.onResponse(json) //handler? -> 핸들러가 null이 아닐때만 실행
+
+                }
+
+            })
+
+
+        }
         //로그인 API를 호출해주는 기능  (mContext아님/ ~Handler? = null이 담길수도 있는 가능성을 열어둠)
         //1. 화면에서 어떤 데이터를 받아와야 하는지? email, pw (함수의 재료로)
         fun postRequestLogin(
