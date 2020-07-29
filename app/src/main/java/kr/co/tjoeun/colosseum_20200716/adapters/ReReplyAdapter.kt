e3b331e3d69fded7b80ee70a8cd9db8f00a71d99
jykim <kjymonster@naver.com>
2020-07-29 19:57:50 +0900

    좋아요/싫어요 서버 연결, 버튼 배경/글씨 색 상황에 맞게 변경

--- app/src/main/java/kr/co/tjoeun/colosseum_20200716/adapters/ReReplyAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/adapters/ReReplyAdapter.kt
@@ -17,9 +17,11 @@ import kr.co.tjoeun.colosseum_20200716.utils.TimeUtil
 import org.json.JSONObject
 import java.util.logging.Handler
 
-class ReReplyAdapter (val mContext: Context,
-                      resId : Int,
-                      val mList:List<Reply>) : ArrayAdapter<Reply>(mContext,resId,mList) {
+class ReReplyAdapter(
+    val mContext: Context,
+    resId: Int,
+    val mList: List<Reply>
+) : ArrayAdapter<Reply>(mContext, resId, mList) {
 
     val inf = LayoutInflater.from(mContext)
 
@@ -53,45 +55,71 @@ class ReReplyAdapter (val mContext: Context,
         likeBtn.text = "좋아요 ${data.likeCount}"
         dislikeBtn.text = "싫어요 ${data.dislikeCount}"
 
+        //좋아요/싫어요 여부에 따른 색 설정
+        if (data.myLike) {
+            likeBtn.setBackgroundResource(R.drawable.red_border_box)
+            likeBtn.setTextColor(mContext.resources.getColor(R.color.naverRed))
 
-      //좋아요 /싫어요 둘다, 실행하는 코드는 동일함.
+        } else {
+            likeBtn.setBackgroundResource(R.drawable.gray_border_box)
+            likeBtn.setTextColor(mContext.resources.getColor(R.color.textGray))
+        }
+
+        if (data.myDisLike) {
+            dislikeBtn.setBackgroundResource(R.drawable.blue_border_box)
+            dislikeBtn.setTextColor(mContext.resources.getColor(R.color.naverBlue))
+
+        }else {
+            dislikeBtn.setBackgroundResource(R.drawable.gray_border_box)
+            dislikeBtn.setTextColor(mContext.resources.getColor(R.color.textGray))
+        }
+
+
+        //좋아요 /싫어요 둘다, 실행하는 코드는 동일함.
         // 서버에 true /false를 보내는지, 보내주는 값만 다를 뿐.
         //두개의 버튼이 눌리면 할일(object : ??) 을 변수에 담아두고, 버튼에게 붙여만 주자.
         //onClickListner : view.view
-        val sendLikeOrDislikeCode = View.OnClickListener{
+        val sendLikeOrDislikeCode = View.OnClickListener {
 
             //서버에 좋아요/싫어요 중 하나를 보내주자.
             //it에 달린 태그값을 Boolean으로 변환해서 좋아요/싫어요를 구별하자.
             //tag는 곧바로 불린으로 못 받아들이므로, String으로 변환 후, Boolean으로 변환.
-            ServerUtil.postRequestLikeOrDisLike(mContext,data.id, it.tag.toString().toBoolean(), object : ServerUtil.JsonResponseHandler{
-                override fun onResponse(json: JSONObject) {
+            ServerUtil.postRequestLikeOrDisLike(
+                mContext,
+                data.id,
+                it.tag.toString().toBoolean(),
+                object : ServerUtil.JsonResponseHandler {
+                    override fun onResponse(json: JSONObject) {
 
-                    val dataObj = json.getJSONObject("data")
+                        val dataObj = json.getJSONObject("data")
 
-                    val reply = Reply.getReplyFromJson(dataObj.getJSONObject("reply"))
+                        val reply = Reply.getReplyFromJson(dataObj.getJSONObject("reply"))
 
-                    data.likeCount = reply.likeCount
-                    data.dislikeCount = reply.dislikeCount
-                    data.myLike = reply.myLike
-                    data.myDisLike = reply.myDisLike
+                        data.likeCount = reply.likeCount
+                        data.dislikeCount = reply.dislikeCount
+                        data.myLike = reply.myLike
+                        data.myDisLike = reply.myDisLike
 
-                    //안드로이드 제공 핸들러 사용.
-                    val uiHandler = android.os.Handler(Looper.getMainLooper())
-                    uiHandler.post {
-                        notifyDataSetChanged()  //새로고침
+                        //안드로이드 제공 핸들러 사용.
+                        val uiHandler = android.os.Handler(Looper.getMainLooper())
+                        uiHandler.post {
+                            notifyDataSetChanged()  //새로고침
+                        }
+
+
                     }
 
 
-                }
-
-
-            })
+                })
 
         }
 
         //좋아요/싫어요 버튼이 클릭되면 -> sendLikeOrDislikeCode 내부의 내용을 실행하게 하자.
         likeBtn.setOnClickListener(sendLikeOrDislikeCode)
         dislikeBtn.setOnClickListener(sendLikeOrDislikeCode)
+
+
+
 
 
 
