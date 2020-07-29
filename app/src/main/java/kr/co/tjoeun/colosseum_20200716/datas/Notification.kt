9de608ac82b55583d299e018a55a5dec6c1c8f57
jykim <kjymonster@naver.com>
2020-07-29 20:59:01 +0900

    알림데이터 + XML 파일 추가 -> 리스트뷰를 완성해보자.

---
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/datas/Notification.kt
@@ -0,0 +1,12 @@
+package kr.co.tjoeun.colosseum_20200716.datas
+
+import java.util.*
+
+class Notification {
+
+    var id = 0
+    var title = ""
+
+    //알림이 발생한 시간을 기록할 Calendar 변수
+    val createdAtCal = Calendar.getInstance()
+}
